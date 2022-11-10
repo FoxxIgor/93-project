3c43e8feb73ff436135e9c9b557913f7acb3b223
FoxxIgor <110695245+FoxxIgor@users.noreply.github.com>
2022-11-09 22:37:31 -0300

    Add files via upload

--- room.js
+++ room.js
@@ -1,6 +1,16 @@
-const firebaseConfig = { apiKey: "AIzaSyBs_5N5-A4oTe1DBoJIcGQuI9wceZjdhvM", authDomain: "atividade94-96b4a.firebaseapp.com", databaseURL: "https://atividade94-96b4a-default-rtdb.firebaseio.com", projectId: "atividade94-96b4a", storageBucket: "atividade94-96b4a.appspot.com", messagingSenderId: "139459445547", appId: "1:139459445547:web:d19aa5039b599264897b6e" }; 
-
+const firebaseConfig = {
+    apiKey: "AIzaSyD8kjMGMCb8dtiwS4QSWWtloPO0nyOcwCI",
+    authDomain: "project93-df98f.firebaseapp.com",
+    databaseURL: "https://project93-df98f-default-rtdb.firebaseio.com",
+    projectId: "project93-df98f",
+    storageBucket: "project93-df98f.appspot.com",
+    messagingSenderId: "453418213369",
+    appId: "1:453418213369:web:1b82438e6ceb318a2e6728"
+  };
+var userName = localStorage.getItem("nome");
+document.getElementById("boasVindas").innerHTML="Bem Vindo "+userName+"!!"
 firebase.initializeApp(firebaseConfig); 
+
 
 function adicionar(){
     roomName = document.getElementById("adicionarSalasInput").value;
@@ -8,17 +18,28 @@ function adicionar(){
         sala:"sala adicionada"
     });
     localStorage.setItem("sala", roomName);
-    window.location="conversa.html";
 }
 
 function getData(){
-    firebase.database().ref("/").om('value', function(snapshot){
+    firebase.database().ref("/").on('value', function(snapshot){
         document.getElementById("output").innerHTML="";
         snapshot.forEach(function(childSnapshot){
             childKey=childSnapshot.key;
-            console.log(roomName);
-            row="<div class='roomName id="+roomName+"onclick='redirecionarParaSala(this.id)' > #"+roomName+"</div> <hr>";
-            document.getElementById("output").innerHTML+=row;
+            roomNames=childKey;
+            console.log(roomNames);
+            row="<div class='roomName' id='+roomNames+' onclick='redirecionarParaSala(this.id)'>#"+roomNames+"</div> <hr >";
+            document.getElementById("output").innerHTML+= row;
         });
     });
+}
+function redirecionarParaSala(nomeSala){
+    console.log(nomeSala);
+    localStorage.setItem("roomName", nomeSala);
+    window.location="conversa.html";
+}
+getData();
+function logout(){
+    localStorage.removeItem("name");
+    localStorage.removeItem("sala");
+    window.location="index.html";
 }
